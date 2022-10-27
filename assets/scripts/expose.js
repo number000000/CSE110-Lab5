29464119d95d4fbca7f6a7c443034d33a9c164ae
number000000 <maggieliulmh@gmail.com>
2022-10-27 13:24:44 -0700

    load correct image and sound files

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,35 +3,41 @@ window.addEventListener('DOMContentLoaded', init);
 
 
 function init() {
-  const horn_select = document.getElementById("horn-select");
-  horn_select.addEventListener('change', (event) => {
+
+  var horn_select = document.getElementById("horn-select");
+  horn_select.addEventListener('change', function() {
     switch(horn_select.value){
       case "air-horn":
-        console.log("1"); //FIXME
+        //displaying the correct image
         var air_horn = document.querySelector("img");
-        air_horn.src = 'assets/images/air-horn.svg';
-        /*var air_horn = new Image();
-        air_horn.src = 'assets/images/air-horn.svg';
-        air_horn.onload = function () {
-          document.querySelector("img").setAttribute('src', this.src);
-        };*/
+        air_horn.src = "assets/images/air-horn.svg";
+        //loading the correct audio file
+        air_horn = document.querySelector("audio");
+        air_horn.src = "assets/audio/air-horn.mp3";
+        air_horn.play(); //FIXME
         break;
       case "car-horn":
-        console.log("2"); //FIXME
-        var image = new Image();
-        image.src = 'assets/images/car-horn.svg';
-        image.onload = function () {
-          document.querySelector("img").setAttribute('src', this.src);
-        };
+        var car_horn = document.querySelector("img");
+        car_horn.src = "assets/images/car-horn.svg";
+        //loading the correct audio file
+        car_horn = document.querySelector("audio");
+        car_horn.src = "assets/audio/car-horn.mp3";
+        car_horn.play(); //FIXME
         break;
       case "party-horn":
-        console.log("3"); //FIXME
-        var image = new Image();
-        image.src = 'assets/images/party-horn.svg';
-        image.onload = function () {
-          document.querySelector("img").setAttribute('src', this.src);
-        };
+        var party_horn = document.querySelector("img");
+        party_horn.src = "assets/images/party-horn.svg";
+        //loading the correct audio file
+        party_horn = document.querySelector("audio");
+        party_horn.src = "assets/audio/party-horn.mp3";
+        party_horn.play(); //FIXME
         break;
     }
   })
+
+  //play sound
+  /*var sound_player = document.querySelector(button);
+  sound_player.addEventListener('click', function(){
+    
+  })*/
 }
