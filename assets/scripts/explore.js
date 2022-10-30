55787784d00060503971ee5fb1e4966d891799d6
number000000 <maggieliulmh@gmail.com>
2022-10-30 12:19:57 -0700

    Populate a list of voices

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,25 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  getVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = getVoiceList;
+  }
+}
+
+function getVoiceList(){
+  const voices = speechSynthesis.getVoices();
+  for(let i = 0; i < voices.length; i++){
+    let option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
 }
